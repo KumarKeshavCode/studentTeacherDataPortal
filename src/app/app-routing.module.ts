ccce5bc003cab836e30600f708928d462b846c98
KumarKeshavCode <kumarkeshav03030@gmail.com>
2024-08-22 16:18:53 +0530

    Add: Register form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './dashbord/navbar/navbar.component';
+import { DashbordComponent } from './dashbord/dashbord.component'; 
+import { RegisterComponent } from './dashbord/register/register.component';
 
 const routes: Routes = [
-  {'path': 'navbar',component:NavbarComponent}
+  {'path': 'navbar',component:NavbarComponent},
+  {'path':'',
+        component: RegisterComponent},
+  {
+    'path':'dashboard',
+    component: DashbordComponent,
+   
+    children: [
+    { 'path':'',
+        component: RegisterComponent
+    },
+  ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TeacherComponent } from './dashbord/teacher/teacher.component';
 import { StudentComponent } from './dashbord/student/student.component';
 import { NavbarComponent } from './dashbord/navbar/navbar.component';
 import { DashbordComponent } from './dashbord/dashbord.component';
 import {MatIconModule} from '@angular/material/icon';
-
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-
 import {MatToolbarModule} from '@angular/material/toolbar';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { RegisterComponent } from './dashbord/register/register.component';
 
 @NgModule({
@@ -25,7 +26,7 @@ import { RegisterComponent } from './dashbord/register/register.component';
     StudentComponent,
     NavbarComponent,
     DashbordComponent,
-    RegisterComponent,
+    RegisterComponent 
     
   ],
   imports: [
@@ -36,6 +37,10 @@ import { RegisterComponent } from './dashbord/register/register.component';
     MatMenuModule,
     MatButtonModule,
     MatToolbarModule,
+    MatSelectModule,
+    FormsModule,
+    MatTableModule,
+    MatInputModule,
     MatFormFieldModule
   ],
   providers: [],
